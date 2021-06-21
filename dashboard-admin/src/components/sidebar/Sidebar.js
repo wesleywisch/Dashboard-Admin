7d4e9f3b9f883d9612ad0c084a5a3bd45194f458
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-21 09:18:47 -0400

    Sidebar pronta

--- dashboard-admin/src/components/sidebar/Sidebar.js
+++ dashboard-admin/src/components/sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './sidebar.css';
-import Logo from './assests/Logo.png'
+import Logo from './assests/avatarFoto.png'
 
 
 export default function Sidebar({ sidebarOpen, closeSidebar }){
@@ -22,69 +22,69 @@ export default function Sidebar({ sidebarOpen, closeSidebar }){
                 ></i>
             </div>
 
-            <div className="SidebarMenu">
+            <div className="sidebarMenu">
                 <div className="sidebarLink activeMenuLink">
                     <i className="fa fa-minus-square"></i>
-                    <Link to="#">Home</Link>
+                    <Link className="Link" to="#">Home</Link>
                 </div>
 
                 <h2>ADMIN</h2>
 
                 <div className="sidebarLink">
                     <i className="fa fa-tachometer"></i>
-                    <Link to="#">Área administrativa</Link>
+                    <Link className="Link" to="#">Área administrativa</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-building"></i>
-                    <Link to="#">Lojas</Link>
+                    <Link className="Link" to="#">Lojas</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-archive"></i>
-                    <Link to="#">Produtos</Link>
+                    <Link className="Link" to="#">Produtos</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-bars"></i>
-                    <Link to="#">Categorias</Link>
+                    <Link className="Link" to="#">Categorias</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-utensils"></i>
-                    <Link to="#">Pedidos</Link>
+                    <Link className="Link" to="#">Pedidos</Link>
                 </div>
 
                 <h2>PESSOAS</h2>
 
                 <div className="sidebarLink">
                     <i className="fa fa-male"></i>
-                    <Link to="#">Administradores</Link>
+                    <Link className="Link" to="#">Administradores</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-user-circle"></i>
-                    <Link to="#">Usuários</Link>
+                    <Link className="Link" to="#">Usuários</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-money-bill-alt"></i>
-                    <Link to="#">Pagamentos e custos</Link>
+                    <Link className="Link" to="#">Pagamentos e custos</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-tasks"></i>
-                    <Link to="#">Plataforma</Link>
+                    <Link className="Link" to="#">Plataforma</Link>
                 </div>
 
                 <div className="sidebarLink">
                     <i className="fa fa-file-alt"></i>
-                    <Link to="#">Politica e privacidade</Link>
+                    <Link className="Link" to="#">Politica e privacidade</Link>
                 </div>
 
                 <div className="sidebarLogout">
                     <i className="fa fa-power-off"></i>
-                    <Link to="#">Logout</Link>
+                    <Link className="Link" to="#">Logout</Link>
                 </div>
 
             </div>
