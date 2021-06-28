932b5a380d366e914afbc70ac935fce64c674b2c
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-28 10:01:37 -0400

    Configurações gerais do backend

--- dashboard-admin/app.js
+++ dashboard-admin/app.js
@@ -1,11 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+// const userRouter = require('./Config/routes/userRouter');
+// const adminRouter = require('./Config/routes/adminRouter');
+
+
+mongoose.connect(process.env.MONGO_CONNECT_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (error) =>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Mongo conectado!');
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'Front')));
 
+app.use('/', express.json(),);
 
+app.use('/admin', express.json(),);
 
 app.listen(3000, () =>{
     console.log('Servidor rodando');
