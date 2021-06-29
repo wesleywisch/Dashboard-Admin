c6a9fbc6b5e3e2597e05dad9cde4a1ead836b01e
wesley_wisch <wesley_wisch@hotmail.com>
2021-06-29 10:09:49 -0400

    Privando rotas e fazendo a parte de login por estado

--- dashboard-admin/src/App.js
+++ dashboard-admin/src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Admin } from './pages/Admin';
 
+import loginContext from './contexts/loginContext';
 
+import PrivateRoute from './pages/rotasPrivadas/PrivateRoute';
+import AcessoNegado from './pages/rotasPrivadas/AcessoNegado';
+
+
+const store = createStore(loginContext);
 
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/admin" component={Admin} />
-      </Switch>
-    </Router>
 
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          
+          <PrivateRoute path="/admin">
+            <Admin />
+          </PrivateRoute>
+
+          <Route path="/negado">
+              <AcessoNegado></AcessoNegado>
+            </Route>
+
+        </Switch>
+      </Router>
+    </Provider>
 
 
   );
--- dashboard-admin/src/pages/Home.js
+++ dashboard-admin/src/pages/Home.js
@@ -1,9 +1,34 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
+import AuthButton from '../components/Button/authButton';
 
 export function Home(){
+    const login = useSelector(state=>state);
+
+    const history = useHistory();
+    const location = useLocation();
+
+    useEffect( () =>{
+        let { from } = location.state || { from: { pathname: "/" } }
+        if(login){
+            history.replace(from)
+        }
+    }, [login])
+
+
     return(
-        <div>
-            Página home
-        </div>
+        <nav className="nav">
+            <h1>Wesley</h1>
+            <div>
+                <ul className="nav-links">
+                    <AuthButton></AuthButton>
+                    <li> <Link to="/">Home</Link> </li>
+                    <li> <Link to="/admin">Admin</Link> </li> 
+                    {/* sempre usamos o Link e não o (a) mesmo do html para evitar que a página toda seja recaregada */}
+                </ul>
+            </div>
+        </nav>
     )
 }
